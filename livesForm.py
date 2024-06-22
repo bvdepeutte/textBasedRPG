6b87f3e68d7e9b8c49e0c661b549add7bc3e3c86
bvdepeutte <bvdepeut@gmail.com>
2024-06-22 20:58:45 +0200

    Cyborg class

--- livesForm.py
+++ livesForm.py
@@ -65,51 +65,26 @@ class Human:
                 return print(f'''You still have {self.health} hp.
                              You still have {self.stamina} stamina and {self.energy} energy''')
 
-class Cyborg:
-    def __init__(self):
-        # default HP can be impacted by many other elements
-        self.health = 120
-        # default Stamina can be impacted by many other elements
-        self.stamina = 120
-        # default energy can be impacted by many other elements
-        self.energy = 100
+class Cyborg(Human):
+     def __init__(self):
+          super().__init__()
+          self.energy = 75
+          
+          # Adaptation of the weaknesses of the Human Class
+          self.weaknesses.remove("fear")
+          self.weaknesses.append("electromagnetic")
 
-        self.caracteristics = {
-            "strength": 1,
-            "agility": 1,
-            "constitution":1,
-            "intelligence": 1,
-            "luck": 1,
-            "persuasion": 1,
-            "stealth": 1,
-            "speed": 1,
-            "intiative": 1
-            }
-        # define if the player can select this class during the character creation
-        self._playable = True
-
-        self.weaknesses = ["poison",
-                    "fire",
-                    "acid",
-                    "electromagnetism",
-                    ]
-        self.strength = ["social",
-                    "adaptative",
-                    "creative",
-                    "partialHealing",
-                    "partialMainteance",
-                    "easyLearner"
-                    ]
-        self._canBeEnhanced = True
+          # Adaptation of the strength of the Human Class
+          self.strength.remove("nonEnergyBase")
+          self.strength.append("partialMaintenance")
     
-    def move(self,direction):
-        if self.stamina > 5:
-            self.stamina -= 5
-            return print(f"You walk to the {direction.lower()}.")
+     def getStatus(self):
+        if self.health < 20:
+            return print(f'''You are low health! You still have {self.health} hp.
+                         You still have {self.stamina} stamina and {self.energy} energy''')
+        elif self.energy < 20:
+             return print(f'''Your energy level is low. You still have {self.energy} energy.
+                          You still have {self.stamina} stamina and {self.health} hp''')
         else:
-            return print(f"Your stamina is too low:{self.stamina}. Refill it before making an action")
-
-    def status(self):
-        if self.health == 0:
-            self.alive = False
-            return print(f"{self.name} is dead!")
+            return print(f'''You still have {self.health} hp.
+                          You still have {self.stamina} stamina and {self.energy} energy''')

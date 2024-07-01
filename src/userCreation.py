83bc98fd6574c3cd34405c81f995363498f35fa7
bvdepeutte <bvdepeut@gmail.com>
2024-07-01 21:29:02 +0200

    select class trader, pilot, farmer

--- src/userCreation.py
+++ src/userCreation.py
@@ -49,21 +49,27 @@ class characterCreation:
         if self.classSelection.lower() == "trader":
             self.slowPrint("What a year. You've made a lot of money thank's to the crusade against the Ennemy but you've almost lost everything. How is it possible?")
             time.sleep(0.5)
-            self.slowPrint("")
-            self.slowPrint("There was a battle, one of the greatest you have ever seen. You are trying to remember it... you don't know why but it hurts, way worse than any physical pain... ")
-            self.slowPrint("And your arm... your arm is killing you even if...")
-            time.sleep(2)
-            self.slowPrint("You are now on a medical ship taking you away, away from Hell. You'll be able to rest, at least for a while...")
-            # total value = 45
+            self.slowPrint("It was supposed to be a quick and painless victory...")
+            time.sleep(2)            
+            self.slowPrint("Now you are save, the medical shuttle will bring you back to safety")
+            self.slowPrint("You'll be able to start the trades as soon as you land in the next statioport")
 
         if self.classSelection.lower() == "pilot":
-            pass
-            # total value = 45
+            self.slowPrint("You check the direction one last time before taking a quick break.")
+            time.sleep(0.5)
+            self.slowPrint("It's all good, you are away from this madness. This assignement was supposed to be a punition, it's a blessing")
+            time.sleep(2)            
+            self.slowPrint("Are they ok?")
+            self.slowPrint("You don't want to think about this, there are already enough sleepless night")
 
         if self.classSelection.lower() == "farmer":
-            pass
-            # total value = 45    
-    
+            self.slowPrint("What has happened? A gold digging colony, the new american dream, what a bullshit")
+            self.slowPrint("You've worked hard for the last 5 years...")
+            time.sleep(1)
+            self.slowPrint("for what? for nothing...")            
+            self.slowPrint("Anxiety, fear, sadness, you can't take it anymore")
+            self.slowPrint("They better help me or I don't answer to myself anymore...")
+
     def classImpact(self):
         if self.classSelection.lower() == "soldier":
             self.character.caracteristics["strength"] = 7
@@ -75,6 +81,7 @@ class characterCreation:
             self.character.caracteristics["stealth"] = 5
             self.character.caracteristics["speed"] = 6
             self.character.caracteristics["initiative"] = 5
+            # TODO => add traits for each class based on story
             # total value = 40
         
         if self.classSelection.lower() == "trader":

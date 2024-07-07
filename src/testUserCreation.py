2d0411f2512a69d9b26c6846fd15646efe2eb54c
bvdepeutte <bvdepeut@gmail.com>
2024-07-07 11:27:48 +0200

    correction test testUserCreation

--- src/testUserCreation.py
+++ src/testUserCreation.py
@@ -23,15 +23,52 @@ class TestCharacterCreation(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=['cyb',"cyborg","trader"])
-    def test_selectClass(self, mock_input, mock_stdout):
+    def test_selectClassTrader(self, mock_input, mock_stdout):
         selector = characterCreation()
         selector.selectLiveForm()
         selector.selectClass()
-        isTrader = selector.classSelection.lower()
+        isClass = selector.classSelection.lower()
         testList = ["greed","iem","energy"]
         traits = selector.character.traits.getEnabledTraits()
-        self.assertEqual(isTrader, "trader")
+        self.assertEqual(isClass, "trader")
         self.assertListEqual(sorted(testList),sorted(traits))
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=["human","soldier"])
+    def test_selectClassSoldier(self, mock_input, mock_stdout):
+        selector = characterCreation()
+        selector.selectLiveForm()
+        selector.selectClass()
+        isClass = selector.classSelection.lower()
+        testList = ["amputee"]
+        traits = selector.character.traits.getEnabledTraits()
+        self.assertEqual(isClass, "soldier")
+        self.assertListEqual(sorted(testList),sorted(traits))
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=["human","pilot"])
+    def test_selectClassPilot(self, mock_input, mock_stdout):
+        selector = characterCreation()
+        selector.selectLiveForm()
+        selector.selectClass()
+        isClass = selector.classSelection.lower()
+        testList = ["rebel"]
+        traits = selector.character.traits.getEnabledTraits()
+        self.assertEqual(isClass, "pilot")
+        self.assertListEqual(sorted(testList),sorted(traits))
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=["human","farmer"])
+    def test_selectClassPilot(self, mock_input, mock_stdout):
+        selector = characterCreation()
+        selector.selectLiveForm()
+        selector.selectClass()
+        isClass = selector.classSelection.lower()
+        testList = ["asocial"]
+        traits = selector.character.traits.getEnabledTraits()
+        self.assertEqual(isClass, "farmer")
+        self.assertListEqual(sorted(testList),sorted(traits))
+
 
 if __name__ == "__main__":
     unittest.main()
